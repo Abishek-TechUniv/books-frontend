9a3b04a4299722c0fa957d26954a027af58f0bc6
Abishek Aditya <abishekaditya@outlook.com>
2018-02-22 22:07:21 +0530

    Maximum css done

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -4,7 +4,11 @@ import './Sidebar.css';
 
 const Sidebar = () => (
   <div className="Sidebar-container">
-    <button className="Sidebar-home" />
+    <button className="Sidebar-home">Bs</button>
+    <div className="Sidebar-lower">
+      <i className="material-icons Sidebar-icon">refresh</i>
+      <i className="material-icons Sidebar-icon">settings</i>
+    </div>
   </div>
 );
 
