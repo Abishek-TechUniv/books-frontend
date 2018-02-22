b760ea275149d018e4388a6b47855f59d24f837b
Abishek Aditya <abishekaditya@outlook.com>
2018-02-22 16:43:40 +0530

    Added sidebar

---
+++ src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Sidebar from '../Sidebar';
+import Content from '../Content';
+
+const App = () => (
+  <div className="App-container">
+    <Sidebar />
+    <Content />
+  </div>
+);
+
+export default App;
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Sidebar.css';
+
+const Sidebar = () => (
+  <div className="Sidebar-container">
+    <button className="Sidebar-home" />
+  </div>
+);
+
+export default Sidebar;
